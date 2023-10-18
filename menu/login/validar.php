ccb30754f5af6638d3fce2c3a7dfbbc8620bae2f
Polar <132285393+gitpolar@users.noreply.github.com>
2023-10-18 14:28:16 -0500

    Update validar.php

--- menu/login/validar.php
+++ menu/login/validar.php
@@ -4,9 +4,9 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/polar/assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../../polar/assets/css/validar.css">
-    <link rel="shortcut icon" href="/polar/assets/img/extras/1logo.png">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/validar.css">
+    <link rel="shortcut icon" href="/assets/img/extras/1logo.png">
     <title>Inicio de sesión</title>
   </head>
 
@@ -16,7 +16,7 @@
         <div class="col-md-5">
           <h3 class="login">Inicia sesión como administrador</h3>
           <br>
-          <form action="/polar/menu/login/verificar.php" method="POST">
+          <form action="/menu/login/verificar.php" method="POST">
             <div class="mb-3">
               <label for="name" class="form-label">Usuario:</label>
               <input type="text" class="form-control" id="name" name="name" placeholder="Escribe tu usuario" required>
@@ -27,13 +27,13 @@
             </div>
             <div class="m-4 text-center">
               <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-              <a class="btn btn-secondary" href="/polar/php/index.php" role="button">Regresar</a>
+              <a class="btn btn-secondary" href="/php/index.php" role="button">Regresar</a>
             </div>
           </form>
         </div>
       </div>
     </div>
 
-    <script src="/polar/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
