64bc27718b898affc518a14e2f32049c0266365b
Polar <132285393+gitpolar@users.noreply.github.com>
2023-10-18 14:49:55 -0500

    Update inicio.php

--- administrador/categorias/inicio.php
+++ administrador/categorias/inicio.php
@@ -13,7 +13,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-info">
         <div class="container">
-            <a class="navbar-brand" href="/polar/administrador/page.php">
+            <a class="navbar-brand" href="/administrador/page.php">
                 <img src="/assets/img/extras/1logo.png" alt="Logo" class="logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
