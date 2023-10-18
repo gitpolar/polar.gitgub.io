9fa47b8c12a2245965f407f3995c2bd109e6f9d3
Polar <132285393+gitpolar@users.noreply.github.com>
2023-10-18 14:44:55 -0500

    Update inicio.php

--- administrador/categorias/inicio.php
+++ administrador/categorias/inicio.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/polar/assets/img/extras/1logo.png">
-    <link rel="stylesheet" href="/polar/assets/css/index.css">
-    <link rel="stylesheet" href="/polar/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="/assets/img/extras/1logo.png">
+    <link rel="stylesheet" href="/assets/css/index.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <title>Categorías</title>
 </head>
 
@@ -14,7 +14,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-info">
         <div class="container">
             <a class="navbar-brand" href="/polar/administrador/page.php">
-                <img src="/polar/assets/img/extras/1logo.png" alt="Logo" class="logo">
+                <img src="/assets/img/extras/1logo.png" alt="Logo" class="logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -26,13 +26,13 @@
                             Gestión Polar
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="dataDropdown">
-                            <li><a class="dropdown-item" href="/polar/administrador/usuarios/inicio.php">Usuarios</a></li>
-                            <li><a class="dropdown-item" href="/polar/administrador/categorias/inicio.php">Categorías</a></li>
-                            <li><a class="dropdown-item" href="/polar/administrador/productos/inicio.php">Productos</a></li>
+                            <li><a class="dropdown-item" href="/administrador/usuarios/inicio.php">Usuarios</a></li>
+                            <li><a class="dropdown-item" href="/administrador/categorias/inicio.php">Categorías</a></li>
+                            <li><a class="dropdown-item" href="/administrador/productos/inicio.php">Productos</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/polar/index.php">Cerrar Sesión</a>
+                        <a class="nav-link" href="/php/index.php">Cerrar Sesión</a>
                     </li>
                 </ul>
             </div>
@@ -42,8 +42,8 @@
     <div class="container my-5">
         <h2 class="text-center">Lista de categorías</h2>
         <div class="text-center">
-            <a class="btn btn-success"href="/polar/administrador/categorias/create.php" role="button">Nueva Categoría</a>
-            <a class="btn btn-secondary" href="/polar/administrador/page.php" role="button">Regresar</a>
+            <a class="btn btn-success"href="/administrador/categorias/create.php" role="button">Nueva Categoría</a>
+            <a class="btn btn-secondary" href="/administrador/page.php" role="button">Regresar</a>
         </div>
         <br>
         <br>
@@ -56,7 +56,7 @@
             </thead>
             <tbody>
             <?php
-                include ("../../../polar/php/conexion.php");
+                include ("/php/conexion.php");
 
                 $sql = "SELECT * FROM categorias ";
                 $result = $conexion->query($sql);
@@ -71,8 +71,8 @@
                         <td>$row[idCategoria]</td>
                         <td>$row[nomCategoria]</td>
                         <td>
-                            <a class='btn btn-primary btn-sm' href='/polar/administrador/categorias/edit.php?id=$row[idCategoria]'>Editar</a>
-                            <a class='btn btn-danger btn-sm' href='/polar/administrador/categorias/delete.php?id=$row[idCategoria]'>Eliminar</a>
+                            <a class='btn btn-primary btn-sm' href='/administrador/categorias/edit.php?id=$row[idCategoria]'>Editar</a>
+                            <a class='btn btn-danger btn-sm' href='/administrador/categorias/delete.php?id=$row[idCategoria]'>Eliminar</a>
                         </td>
                     </tr>
                     ";
@@ -82,6 +82,6 @@
         </table>
     </div>
     
-<script src="/polar/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
